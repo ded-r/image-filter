c18976fb9dd94e6a4a51a1f14b2a005a7525aff2
Didar Auyesbay <didarmaratov04@gmail.com>
2024-11-19 20:25:14 +0500

    better ui

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
                 <p className="text-3xl text-center">
                     <b>Image Filter App</b>
                 </p>
-                <form onSubmit={handleSubmit} className="mt-20 text-center">
+                <form onSubmit={handleSubmit} className="mt-20 flex flex-col justify-center items-center align-middle space-y-5 lg:flex-row lg:space-y-0">
                     <input type="file" onChange={handleFileChange} accept="image/*" />
                     <button type="submit" className="border border-white py-2 px-4 hover:bg-white hover:text-black transition ease-in-out duration-300">
                         Apply Filters
@@ -48,11 +48,11 @@ function App() {
                 </form>
                 {message && <p>{message}</p>}
                 {filteredImages && (
-                    <div className="grid grid-cols-3 mt-20">
+                    <div className="grid lg:grid-cols-3 mt-20">
                         {Object.entries(filteredImages).map(([filterName, base64Image]) => (
-                            <div key={filterName}>
+                            <div key={filterName} className="flex flex-col items-center justify-center">
                                 <h3>{filterName}</h3>
-                                <img src={`data:image/png;base64,${base64Image}`} alt={filterName} style={{ width: "200px", height: "auto", margin: "10px" }} />
+                                <img width={450} height={400} className="m-10" src={`data:image/png;base64,${base64Image}`} alt={filterName} />
                             </div>
                         ))}
                     </div>
